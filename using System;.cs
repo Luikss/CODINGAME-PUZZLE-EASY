b8265c3227dabdf9c51207e452dcda64bb79f6e1
Andre Luige <78085382+Luikss@users.noreply.github.com>
2021-01-28 17:14:10 +0200

    Create using System;.cs

---
+++ using System;.cs
@@ -0,0 +1,26 @@
+using System;
+using System.Linq;
+using System.IO;
+using System.Text;
+using System.Collections;
+using System.Collections.Generic;
+
+class Player {
+
+    static void Main(string[] args) {
+
+        while (true) {          
+            int max = 0;
+            int imax = 0;
+            for (int i = 0; i < 8; i++) {
+
+                int mountainH = int.Parse(Console.ReadLine());
+                if (mountainH > max) {
+                    max = mountainH;
+                    imax = i;
+                }
+            }
+            Console.WriteLine(imax);
+        }
+    }
+}
